56c4420970500237c00720ef0652d1ced31cba89
JamesLi0217 <pli14@stevens.edu>
2020-03-14 22:11:15 -0400

    Initial commit

---
+++ app/src/main/java/com/laioffer/matrix/User.java
@@ -0,0 +1,34 @@
+package com.laioffer.matrix;
+
+public class User {
+
+    private double user_timestamp;
+
+    public double getUser_timestamp() {
+        return user_timestamp;
+    }
+
+    public void setUser_timestamp(double user_timestamp) {
+        this.user_timestamp = user_timestamp;
+    }
+
+    private String user_account;
+
+    public String getUser_password() {
+        return user_password;
+    }
+
+    public void setUser_password(String user_password) {
+        this.user_password = user_password;
+    }
+
+    private String user_password;
+
+    public String getUser_account() {
+        return user_account;
+    }
+
+    public void setUser_account(String user_account) {
+        this.user_account = user_account;
+    }
+}
